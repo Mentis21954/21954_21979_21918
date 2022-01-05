71a1503b851095a7370fd6980741e9721eb3c4b2
Mentis21954 <80312951+Mentis21954@users.noreply.github.com>
2022-01-05 14:09:23 +0200

    Delete project directory

--- project/src/main/java/com/springboot/app/Repository/LessonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.app.Repository;
-
-import com.springboot.app.Entity.Lesson;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LessonRepository extends JpaRepository<Lesson, Integer> {
-}
